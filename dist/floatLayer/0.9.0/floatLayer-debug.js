4f42d91870f8fdda3ad533edf7d8ccb1966a0e5a
mui-lychi <qiqiyoujian@gmail.com>
2012-06-28 21:00:59 +0800

    udpate handy

--- dist/floatLayer/0.9.0/floatLayer-debug.js
+++ dist/floatLayer/0.9.0/floatLayer-debug.js
@@ -1,78 +1,82 @@
 // floatLayer
 // =======
 // 提供层的浮动、固定功能
-define("#floatLayer/0.9.0/floatLayer-debug", ["overlay"], function(require, exports, module) {
-    var Overlay = require('overlay');
-
+define("#floatLayer/0.9.0/floatLayer-debug", [], function(require, exports, module) {
     // thanks to http://cubiq.org/followalong
-    var followAlong = function (el, options) {
-            var that = this,
+    var has3d = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix()),
+        translateOpen = 'translate' + (has3d ? '3d(' : '('),
+        translateClose = has3d ? ',0)' : ')';
+
+    function FloatLayer(options) {
+            var self = this,
                 i;
 
             // Default options
-            that.options = {
-                duration: '100ms'
+            self.options = {
+                duration: '100'
             };
 
             // User defined options
             if (typeof options == 'object') {
                 for (i in options) {
-                    that.options[i] = options[i];
+                    self.options[i] = options[i];
                 }
             }
 
-            that.element = typeof el == 'object' ? el : document.querySelector(el);
-            that.element.style.webkitTransitionProperty = '-webkit-transform';
-            that.element.style.webkitTransitionTimingFunction = 'cubic-bezier(0,0,0.25,1)';
-            that.element.style.webkitTransitionDuration = that.element.style.webkitTransitionDuration = that.options.duration;
-            that.element.style.webkitTransform = translateOpen + '0,0' + translateClose;
+            var el = options['element'];
+            var el = typeof el == 'object' ? el : document.querySelector(el),
+                dur = el.style.webkitTransitionDuration;
 
-            el = that.element;
-            that.x1 = that.x2 = that.y1 = that.y2 = 0;
+            el.style.webkitTransitionProperty = '-webkit-transform';
+            el.style.webkitTransitionTimingFunction = 'cubic-bezier(0,0,.25,1)';
+            el.style.webkitTransitionDuration=dur=self.options.duration + 'ms';
+            el.style.webkitTransform = translateOpen + '0,0' + translateClose;
+
+            self.element = el;
+
+            self.x1 = self.x2 = self.y1 = self.y2 = 0;
+
             do {
-                that.x1 += el.offsetLeft;
-                that.y1 += el.offsetTop;
+                self.x1 += el.offsetLeft;
+                self.y1 += el.offsetTop;
             } while (el = el.offsetParent);
 
-            that.x2 = that.x1 + that.element.offsetWidth;
-            that.y2 = that.y1 + that.element.offsetHeight;
-
-            setTimeout(function () {
-                that.follow();
+            setTimeout(function() {
+                self.sync();
             }, 0);
 
-            window.addEventListener('scroll', that, false);
-        },
-        has3d = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix()),
-        translateOpen = 'translate' + (has3d ? '3d(' : '('),
-        translateClose = has3d ? ',0)' : ')';
-
-
-    followAlong.prototype = {
-        handleEvent: function (e) {
-            if (e.type == 'scroll') {
-                this.follow(e);
-            }
-        },
-
-        follow: function (e) {
-            var that = this,
-                scrollX = window.scrollX,
-                scrollY = window.scrollY;
-
-            if (window.scrollX > that.x1 || window.scrollY > that.y1) {
-                that.element.className = that.element.className ? that.element.className + ' float' : 'float';
-                that.element.style.left = that.x1 + 'px';
-                that.element.style.top = that.y1 + 'px';
-                that.element.style.webkitTransform = translateOpen + scrollX + 'px,' + scrollY + 'px' + translateClose;
-            } else {
-                that.element.style.webkitTransform = translateOpen + '0,0' + translateClose;
-                that.element.className = that.element.className.replace(/(^|\\s)float(\\s|$)/gi, '');
-                that.element.style.left = '';
-                that.element.style.top = '';
-            }
+            window.addEventListener('scroll', self, false);
         }
-    }
 
-    module.exports = followAlong;
+    FloatLayer.prototype = {
+        handleEvent: function(e) {
+            if (e.type == 'scroll') {
+                this.sync(e);
+            }
+        },
+
+        sync: function() {
+            var self = this,
+                scrollX = window.scrollX,
+                scrollY = window.scrollY,
+                el = self.element,
+                transform;
+
+            if (window.scrollX > self.x1 || window.scrollY > self.y1) {
+                el.style.left = self.x1 + 'px';
+                el.style.top = self.y1 + 'px';
+
+                transform = translateOpen + scrollX + 'px,'
+                                + scrollY + 'px' + translateClose;
+            } else {
+                transform = translateOpen + '0,0' + translateClose;
+                el.style.left = '';
+                el.style.top = '';
+            }
+
+            el.style.webkitTransform = transform;
+        }
+    };
+
+    module.exports = FloatLayer;
 });
